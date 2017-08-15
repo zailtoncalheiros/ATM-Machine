fe62183daf0e17d774ca86a10835cadbe0fa07d9
Zailton Calheiros <zailton@ppgsc.ufrn.br>
2017-08-15 18:14:31 -0300

    Add methods to return balance, to add a new operation and to transfer values.

--- src/atm_machine/storage/TransactionDAO.clj
+++ src/atm_machine/storage/TransactionDAO.clj
@@ -1,13 +1,15 @@
-(ns atm-machine.storage.transactionsdao)
+(ns atm-machine.storage.transactionsdao
+  (:require [atm-machine.storage.persondao :as persondao]
+            [clojure.java.jdbc :as db]))
 
-(defprotocol TransactionDAO
+(defprotocol TransactionDAOProtocol
     (transfer! [storage from to value])
-    
-    (get-balance [storage])
+
+    (get-balance [storage ag acc])
 
     (get-statement [storage last-days])
 
-    (perform-operation! [storage value description]))
+    (perform-operation! [storage ag acc value description]))
 
 (def transaction-desc {:table-name "public.transaction"
                     :id "id"
@@ -15,12 +17,50 @@
                     :balance "balance"
                     :value "value"
                     :description "description"
-                    :transaction-time "transaction-time"})
+                    :transaction-time "transaction_time"})
+
+(defn get-balance [spec ag acc]
+  (let [person-id (persondao/get-id spec ag acc)]
+  (let [rows (db/query spec [(str "SELECT * FROM " (:table-name transaction-desc) " WHERE "
+                      (:account transaction-desc) " = " person-id " ORDER BY "
+                      (:transaction-time transaction-desc) " DESC LIMIT 1")])]
+    (if (empty? rows) nil ((:balance transaction-desc) (first rows))))))
+
+(defn perform-operation! [spec ag acc value description]
+  (db/with-db-transaction [trans-conn spec]
+    (let [person-id (persondao/get-id trans-conn ag acc)]
+    (let [balance (get-balance trans-conn ag acc)]
+    (let [new-balance (if (nil? balance) 0 (+ balance value))]
+    (let [insert-query (str "INSERT INTO " (:table-name transaction-desc) " ("
+                            (:account transaction-desc) ", "
+                            (:balance transaction-desc) ", "
+                            (:value transaction-desc) ", "
+                            (:description transaction-desc) ", "
+                            (:transaction-time transaction-desc) ") VALUES ("
+                            person-id ", "
+                            new-balance ", "
+                            value ", "
+                            description ", "
+                            "CURRENT_TIMESTAMP)")]
+      (db/execute! trans-conn [insert-query])))))))
+
+(defn transfer! [spec from-ag from-acc to-ag to-acc value]
+  (db/with-db-transaction [trans-conn spec]
+    (let [from-id (persondao/get-id trans-conn from-ag from-acc)
+          to-id (persondao/get-id trans-conn to-ag to-acc)]
+    (let [from-row-query (str "SELECT * FROM " (:table-name transaction-desc) " WHERE "
+                              (:id transaction-desc) " = " from-id)]
+    (let [from-row (first (db/query trans-conn from-row-query))]
+      (if (< ((:value transaction-desc) from-row) value)
+        nil
+        ((perform-operation! trans-conn from-ag from-acc (- value) (str "Transference to Ag:" to-ag ", Acct: " to-acc))
+         (perform-operation! trans-conn to-ag to-acc value (str "Transference from Ag:" from-ag ", Acct: " from-acc)))))))))
+
 
 (defn create-transaction-table! [spec]
   (db/execute! spec [(str "CREATE TABLE IF NOT EXISTS " (:table-name transaction-desc) "( "
                               (:id transaction-desc) " BIGSERIAL PRIMARY KEY, "
-                              (:account transaction-desc) " BIGINT NOT NULL REFERENCES " (:table-name persondao.person-desc) ", "
+                              (:account transaction-desc) " BIGINT NOT NULL REFERENCES " (:table-name persondao/person-desc) ", "
                               (:balance transaction-desc) " BIGINT NOT NULL, "
                               (:value transaction-desc) " BIGINT NOT NULL, "
                               (:description transaction-desc) " TEXT NOT NULL ",
